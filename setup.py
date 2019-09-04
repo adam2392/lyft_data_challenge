de4bccb5977c6761defda50f63e81719ba2d3aa8
Adam Li <adam2392@gmail.com>
2019-09-04 10:43:31 -0400

    Initial cookie cutter commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A lyft data science challenge from 2019.',
+    author='Adam Li',
+    license='MIT',
+)
